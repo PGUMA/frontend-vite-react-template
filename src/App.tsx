6115d11cf44ee829ea736b160a2f4c2cdc582853
pguma <p.guma.service+git@gmail.com>
2024-02-07 23:33:41 +0900

    mod demo App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
 import { useState } from "react";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
 
-function App() {
-	const [count, setCount] = useState(0);
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
 
-	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	);
-}
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+    console.log("input value:", inputValue);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="input text"
+      />
+
+      <button type="button" onClick={handleButtonClick}>
+        click
+      </button>
+    </div>
+  );
+};
 
 export default App;
